f27a9676db0e3e9115772d9df6c5d16b09233aac
dendariev <den.dariev@gmail.com>
2022-11-28 00:04:23 +0800

    Задание 1.2

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        // Задание 1
-        System.out.println("Задание 1");
+        // Задание 1.1
+        System.out.println("Задание 1.1");
         int deposit = 15000;
         int total = 0;
         int month = 0    ;
@@ -11,6 +11,15 @@ public class Main {
             System.out.println("Месяц " +month+ ", сумма накоплений равна " +total);
         }
         System.out.println("Месяц " +month+ ", сумма накоплений равна " +total+ " рублей");
-
+        // Задание 1.2
+        System.out.println("Задание 1.2");
+        int i = 1;
+        while (i <= 10) {
+            System.out.print(i++);
+        }
+        System.out.println();
+        for (i = 10; i >= 1; i--) {
+            System.out.print(i);
+        }
     }
 }
