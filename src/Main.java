614e7a57e933aded409cfae209a8391284398d64
dendariev <den.dariev@gmail.com>
2022-11-27 23:50:26 +0800

    ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Задание 1
+        System.out.println("Задание 1");
+        int deposit = 15000;
+        int total = 0;
+        int month = 0    ;
+        while (total < 2_459_000) {
+            month++;
+            total = total + deposit;
+            System.out.println("Месяц " +month+ ", сумма накоплений равна " +total);
+        }
+        System.out.println("Месяц " +month+ ", сумма накоплений равна " +total+ " рублей");
+
     }
 }
